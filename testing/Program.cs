948d0a5610b21ed980755cce624b568068c3ae96
nshawkins <hawkinsn67@gmail.com>
2021-04-28 12:18:33 -0400

    new connection

--- testing/Program.cs
+++ testing/Program.cs
@@ -34,7 +34,7 @@ namespace Server
             database + ";" + "User Id=" + uid + ";" + "Password=" + password + ";";*/
 
             //server = "tcp:3.23.163.170,3306";
-            server = "tcp:52.14.191.216";
+            server = "tcp:127.0.0.1,3306";
             //Library = "DBMSSOCN"; 
             database = "herdrdb";
 
